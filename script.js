88271dc862932d828f2a02bd734d524780652bb5
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2024-11-22 22:17:32 +0100

    added stars icons to rating

--- script.js
+++ script.js
@@ -181,10 +181,10 @@ function canvasRating(rating) {
   const halfRating = String(rating).indexOf('.');
   let html = '';
   for (let i = 0; i < rating; i ++) {
-    html += `<span>star</span>`;
+    html += `<span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m233-120 93-304L80-600h304l96-320 96 320h304L634-424l93 304-247-188-247 188Z"/></svg></span>`;
   }
   if (halfRating !== -1) {
-    html += `<span>halfstar</span>`;
+    html += `<span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m606-286-33-144 111-96-146-13-58-136v312l126 77ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"/></svg></span>`;
   }
 return html;
 
